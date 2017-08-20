12349979cf04e710701adae580fd3345744d545e
Jufebrown <jufebrown@gmail.com>
2017-08-19 23:18:04 -0500

    added loadChallenge() to challengeFactory.js

--- app/factories/challengeFactory.js
+++ app/factories/challengeFactory.js
@@ -1,5 +1,14 @@
 'use strict'
 
 app.factory('authFactory', function($http) {
-  
+  return {
+
+    loadChallenge: function(challengeNum) {
+      $http.get('app/assets/challenges/challenges.json')
+       .then(function(res){
+          return res.data[challengeNum]
+      })
+    })
+  }
+
 })
