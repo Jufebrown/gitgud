6a1e1005244f86fc7f87a70fce94fc3d5e74d708
Jufebrown <jufebrown@gmail.com>
2017-08-19 23:41:20 -0500

    added added variable assignments after loading json

--- app/controllers/challengeCtrl.js
+++ app/controllers/challengeCtrl.js
@@ -3,5 +3,12 @@
 app.controller('challengeCtrl', function($scope, challengeFactory) {
 
   challengeFactory.loadChallenge($scope.gameState.challengeNum)
+  .then((challengeObj) => {
+    // need to work out how to load in correct answer images
+    $scope.originImage = challengeObj.originImage.initial
+    $scope.localImage = challengeObj.localImage.initial
+    $scope.prompt = challengeObj.prompt
+    $scope.answer = challengeObj.answer
+  })
 
 })
