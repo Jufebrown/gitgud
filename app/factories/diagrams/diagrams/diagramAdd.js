d2c9536875c6a8f37d63cab2a8c5e1e92abc0e2a
daniel-certa-1228 <mfdancerta@gmail.com>
2017-08-20 16:41:17 -0500

    Added diagram ADD and CHECKOUT -B

--- app/factories/diagrams/diagrams/diagramAdd.js
+++ app/factories/diagrams/diagrams/diagramAdd.js
@@ -1,6 +1,240 @@
 app.factory('diagramAdd', function (diagramStart) {
+const initialOrigin = () => {
+    var graph = diagramStart('origin-canvas')
+    graph.render();
+
+    let master = graph.branch({
+      name : "master",
+      color: "blue"
+    });
+
+    master
+      .commit({
+        message: "initial commit",
+        dotColor: "blue"
+      })
+      .commit({
+        message: "second commit",
+        dotColor: "blue"
+      })
+
+    let featureOrange = master.branch({
+      name: "feature-orange",
+      color: "orange",
+      lineColor: "orange",
+    })
+
+    master
+      .commit({
+        message: "third commit",
+        dotColor: "blue"
+      })
+      .commit({
+        message: "fourth commit",
+        dotColor: "blue"
+      })
+
+    featureOrange
+      .commit({
+        message: "added some cool styles",
+        dotColor: "orange"
+      })
+      .commit({
+        message: "fixed button errors",
+        dotColor: "orange"
+      })
+  }
+
+  const initialLocal = () => {
+    var graph = diagramStart('local-canvas')
+    graph.render();
+
+    let master = graph.branch({
+      name : "master",
+      color: "blue"
+    });
+
+    master
+      .commit({
+        message: "initial commit",
+        dotColor: "blue"
+      })
+      .commit({
+        message: "second commit",
+        dotColor: "blue"
+      })
+
+    let featureOrange = master.branch({
+      name: "feature-orange",
+      color: "orange",
+      lineColor: "orange",
+    })
+
+    master
+      .commit({
+        message: "third commit",
+        dotColor: "blue"
+      })
+      .commit({
+        message: "fourth commit",
+        dotColor: "blue"
+      })
+
+    featureOrange
+      .commit({
+        message: "added some cool styles",
+        dotColor: "orange"
+      })
+      .commit({
+        message: "fixed button errors",
+        dotColor: "orange"
+      })
+
+    let featureYellow = featureOrange.branch({
+      	name: "feature-yellow",
+      	color: "yellow",
+      	lineColor: "yellow"
+      })
+
+      featureOrange
+      	.commit({
+      		message: "changed google font",
+      		color: "orange",
+      		dotColor: "orange"
+      	})
+
+      featureYellow
+      	.commit({
+      		message: "trying some styling changes",
+      		color: "yellow",
+      		dotColor:"yellow"
+      	})
+      
+  }
+
+  const correctOrigin = () => {
+    var graph = diagramStart('origin-canvas')
+    graph.render();
+
+    let master = graph.branch({
+      name : "master",
+      color: "blue"
+    });
+
+    master
+      .commit({
+        message: "initial commit",
+        dotColor: "blue"
+      })
+      .commit({
+        message: "second commit",
+        dotColor: "blue"
+      })
+
+    let featureOrange = master.branch({
+      name: "feature-orange",
+      color: "orange",
+      lineColor: "orange",
+    })
+
+    master
+      .commit({
+        message: "third commit",
+        dotColor: "blue"
+      })
+      .commit({
+        message: "fourth commit",
+        dotColor: "blue"
+      })
+
+    featureOrange
+      .commit({
+        message: "added some cool styles",
+        dotColor: "orange"
+      })
+      .commit({
+        message: "fixed button errors",
+        dotColor: "orange"
+      })
+  }
+
+
+
+  const correctLocal = () => {
+    var graph = diagramStart('local-canvas')
+    graph.render();
+
+    let master = graph.branch({
+      name : "master",
+      color: "blue"
+    });
+
+    master
+      .commit({
+        message: "initial commit",
+        dotColor: "blue"
+      })
+      .commit({
+        message: "second commit",
+        dotColor: "blue"
+      })
+
+    let featureOrange = master.branch({
+      name: "feature-orange",
+      color: "orange",
+      lineColor: "orange",
+    })
+
+    master
+      .commit({
+        message: "third commit",
+        dotColor: "blue"
+      })
+      .commit({
+        message: "fourth commit",
+        dotColor: "blue"
+      })
+
+    featureOrange
+      .commit({
+        message: "added some cool styles",
+        dotColor: "orange"
+      })
+      .commit({
+        message: "fixed button errors",
+        dotColor: "orange"
+      })
+
+    let featureYellow = featureOrange.branch({
+      	name: "feature-yellow",
+      	color: "yellow",
+      	lineColor: "yellow"
+      })
+
+      featureOrange
+      	.commit({
+      		message: "changed google font",
+      		color: "orange",
+      		dotColor: "orange"
+      	})
+
+      featureYellow
+      	.commit({
+      		message: "trying some styling changes",
+      		color: "yellow",
+      		dotColor:"yellow"
+      	})
+      
+  }
+
   return {
-    initial : () => {},
-    correct : () => {}
+    initial : () => {
+      initialOrigin();
+      initialLocal();
+    },
+    correct : () => {
+      correctOrigin();
+      correctLocal();
+    }
   }
 });
