0ae74c7576da004077e24360ac3b3eb3a2d5fd0a
Jufebrown <jufebrown@gmail.com>
2017-08-19 23:24:13 -0500

    added loadChallenge function call

--- app/controllers/challengeCtrl.js
+++ app/controllers/challengeCtrl.js
@@ -1,5 +1,7 @@
 'use strict'
 
-app.controller('challengeCtrl', function($scope) {
-  
+app.controller('challengeCtrl', function($scope, challengeFactory) {
+
+  challengeFactory.loadChallenge($scope.gameState.challengeNum)
+
 })
